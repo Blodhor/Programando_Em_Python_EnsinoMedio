fb2d725a18409b720b80ffdce32fd1de546d6d69
Bruno Braga <bruno.braga@ufms.br>
2025-02-20 10:53:59 -0400

    Update Aula_6.py

--- Aula_6.py
+++ Aula_6.py
@@ -1,63 +1,53 @@
 # Baseado no tuotrial de https://docs.sympy.org/latest/tutorials/intro-tutorial/index.html
 import sympy as sp
 
-# sympy foi feito para cálculos matemáticos exatos, sem aproximações
-# Exemplo: a raiz de 8
-print(8**0.5) # mostra uma aproximação
-print(sp.sqrt(8)) # a raiz é simplificada e não aproximada
+'''O sympy foi criado para resolver cálculos matemáticos exatos, sem aproximações. Exemplo: a raiz de 8'''
+# o método padrão por potencia aproxima
+print(8**0.5)
+# o sympy apenas simplifica
+print(sp.sqrt(8))
 
-# Podemos também escrever expressões matemáticas
-# definindo as variáveis x e y, não precisamos mais
-#  associar valores a x e y
+'''Podemos também escrever expressões matemáticas definindo as variáveis x e y, não precisamos mais associar valores específicos a x e y'''
 x, y = sp.symbols('x y')
 f = x + 2*y
 print(f)
 # podemos alterar as expressões
 print(f +1 -x)
-# algumas operações não são feitas automaticamente
-#  pois o sympy acha que a expressão como está, é sua
-#  formar simplificada
-'''Exemplo: 
-x*(x+2*y) = x**2 + 2*x*y'''
+
+'''Algumas operações não são feitas automaticamente, pois a função heurística do sympy pode determinar que a expressão como está, já é sua formar simplificada. Exemplo: x*(x+2*y) = x**2 + 2*x*y'''
 f2 = x* f
 print(f2)
 print("Expandindo", sp.expand(f2))
-# de modo inverso podemos ter originalmente uma expressão 
-# como a f2, e podemos fatora-la
+'''De modo inverso podemos ter originalmente uma expressão como x**2 + 2*x*y, e  fatora-la para obter a nossa f2'''
 print("Fatorando", sp.factor(x**2 + 2*x*y))
 
 #Resolvendo equações
 eq1 = x**2 -2
-sol1 = sp.solve(eq1, x) # Iguala eq1 a zero e isole o 'x'
+sol1 = sp.solve(eq1, x) #O solve iguala eq1 a zero e isola o 'x'
 ''' x**2 -2 = 0  -> x? '''
 sol2 = sp.solve(f2,y) # resolva em 'y'
 print("Eq:", eq1, "= 0, solução em x:",sol1)
 print("Eq:", f2, "= 0, solução em y:",sol2)
+
 # podemos substituir uma variavel da equação por um valor específico
 f2s= f2.subs(y, -8/x) # faça y= -8/x
 print("Eq:", f2s, "= 0, solução:",sp.solve(f2s,x))
 
-# não precisamos resolver sempre igualando a zero
-''' podemos fazer por ex.
-x**2 -16= 9, e resolver em x'''
+'''Não precisamos resolver as equações sempre igualando-as a zero. Também conseguimos resolver com outra igualdade. Por exemplo, pedir a eq: x**2 -16= 9, e resolver em x. Para isso usamos o comando Eq que iguala duas expressões.'''
 f2s9 = sp.Eq(f2s, 9) # faz f2s = 9
 print("Eq:", sp.expand(f2s9), "solução:",sp.solve(f2s9,x))
 
-# Finalmente, podemos determinar a solução de um sistema
-#  de equações
-'''
-x-2*y=0
-x+y=18
-'''
+'''Finalmente, podemos determinar a solução de um sistema de equações. Por exemplo {x-2*y=0, x+y=18}'''
 se1 = x-2*y
 se2 = sp.Eq(x+y, 18)
 print("Resolvendo o sistema:\n(1)", se1,"= 0")
 print("(2)",se2)
-solsis=sp.linsolve([se1,se2], (x,y)) # o (x,y) indica tanto as variáveis que queremos solução quanto sua ordem
+'''Usando o comando linsolve, passamos uma lista de equações e enseguida a lista de variáveis que queremos encontrar como (x,y), A solução será entregue na ordem pedida, nesse caso, primeiro x e depois y'''
+solsis=sp.linsolve([se1,se2], (x,y))
 print("(x , y) =",solsis)
-'''se tiver alguma equação não linear no sistema
-ex: x**2 = 10,
-use o método sp.nonlinsolve()'''
+'''Se tivermos alguma equação não linear no sistema (potência maior que 1), ex: x**2 = 10, precisamos usar o método sp.nonlinsolve()'''
+solsis2 = sp.nonlinsolve([x**2-2*y, sp.Eq(6*x+y,-18)], (x,y))
+print("Solução do sistema não linear:", solsis2)
 
 '''
 Atividade 8: Sobre um rio, há uma ponte de 20 metros de altura de onde um
