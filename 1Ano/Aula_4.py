1f258a23a72312170f55f3acb7aacf43d702c59a
Bruno Braga <bruno.braga@ufms.br>
2025-03-06 12:40:39 -0400

    Update Aula_4.py

--- 1Ano/Aula_4.py
+++ 1Ano/Aula_4.py
@@ -1,92 +1,101 @@
-'''Para gerar gráficos precisamos de uma biblioteca
-específica. Caso você esteja usando aplicativos como o
-Google Colab Notebook, todas as bibliotecas já estão instaladas.
 
-Se estiver programando localmente (sem internet), precisara instalar.
-Recomendo instalar o Pip (https://pypi.org/). No Visual Studio Code
-da Microsoft ele é instalado como extensão; no Pydroid de aparelhos
-Android ele já vem preparado.
+'''Para gerar gráficos precisamos de uma biblioteca específica. Caso você esteja usando aplicativos como o Google Colab Notebook, todas as bibliotecas já estão instaladas. Se estiver programando localmente (sem internet), precisara instalar as bibliotecas necessárias.
 
-Com o pip ou algum gerenciador do seu aplicativo instale as bibliotecas:
-    matplotlib
-    numpy
+Recomendo instalar o Pip (https://pypi.org/). No Visual Studio Code da Microsoft ele é instalado como extensão; no Pydroid de aparelhos Android ele já vem preparado.
+
+Com o pip ou algum gerenciador do seu aplicativo instale as bibliotecas:  matplotlib e numpy.
 '''
 
-#Para utilizar as bibliotecas precisamos chama-las
-# abaixo uso "chame a biblioteca numpy, e usaremos ela aqui com o nome 'np'"
+'''Para utilizar as bibliotecas precisamos chama-las. Abaixo uso o comando que chama a biblioteca numpy, e usaremos ela aqui com o nome 'np’. De modo similar chamamos de ‘plt’ a biblioteca genérica de gráficos'''
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Utilizaremos o exemplo de "Fundamentos2" como função
+#Métodos para MUV
 def Posicao_MUV(s0,v0,a):
     return lambda t: s0 + v0*t + (a/2)* t**2
 
-def Grafico(f=Posicao_MUV(0,1,1),pontos=5,eixox='Tempo (s)',eixoy='Altura (m)',delta=0.5,linha=False):
+def Grafico(f=Posicao_MUV(0,1,1), pontos=5, eixox='Tempo (s)', eixoy='Altura (m)', delta=0.5, linha=False):
     '''
-    f:      função que utilizaremos
-    pontos: quantidade de pontos que queremos no gráfico
+    f:        função que utilizaremos
+    pontos: quantidade de pontos no gráfico
     eixox:  nome no eixo x
     eixoy:  nome no eixo y
-    delta:  diferença de pontos consecutivos no eixo x
+    delta:  espaçamento entre pontos consecutivos no eixo x
     linha:
-        se True, constrói o gráfico com uma linha ligando os pontos (fica incorreto com poucos pontos)
-        se False, constrói o gráfico com pontos apenas
-    '''
-    #Precisamos criar a interface com os eixos, para 
-    # isso chamamos o método figure
-    fig = plt.figure(dpi=150) #dpi é a qualidade desejada onde 100 é razoavel e 300 é muito alta
-
-    #Para colocar nossa função no gráfico, basta gerar uma lista de pontos
-    '''Caso f = x**2, precisa-se de alguns valores como:
-        se x=-2, f= 4
+     +se True, constrói o gráfico com uma linha ligando os pontos (fica incorreto com poucos pontos)
+     +se False, constrói o gráfico com pontos apenas'''
+    #Precisamos criar a interface com os eixos
+    # para isso chamamos o método figure
+    fig = plt.figure(dpi=150)
+    '''O dpi é a qualidade desejada onde 100 é razoavel e 300 é muito alta. Para colocar nossa função no gráfico, basta gerar uma lista de pontos. Caso f = x**2, precisa-se de alguns valores como:
+       se x=-2, f= 4
         se x=-1, f= 1
         se x=0, f= 0
         se x=1, f= 1
         se x=2, f= 4
-        Assim teremos as listas:
-        x=[-2,-1,0,1,2] e y=[4,1,0,1,4]
-    '''
-    #nossa função será a dada como 'f'
-    # Aqui assumimos por conveniência que só importa os
-    #  valores positivos do eixo x
-    #Podemos criar uma lista com o seguinte comando
+Assim teremos as listas: x=[-2,-1,0,1,2] e y=[4,1,0,1,4];
+A nossa função será a dada como 'f'. Aqui assumimos por conveniência que só importa os valores positivos do eixo x (tempo). Podemos criar uma lista com o seguinte comando'''
     x=[delta*i for i in range(pontos)]
-    '''se usar '0.5*i for i in range(4)':
-    i assumiria os valores de 0, 1,2,3 então ficariamos com
-    x=[0, 0.5, 1.0, 1.5]'''
+    '''Se usar '0.5*i for i in range(4)' i assumiria os valores de 0,1,2,3 então ficariamos com a lista [0, 0.5, 1.0, 1.5]'''
     y=[]
     for i in x:
-        #para todos os valores de x, queremos os valores de y
+       #para todos os valores de x, queremos os valores de y
         y.append(f(i))
 
     #Para deixar o gráfico mais fácil de ler, daremos nomes aos eixos
     plt.ylabel(eixoy, fontsize=12) # fontzise é o tamanho da letra nas legendas
     plt.xlabel(eixox, fontsize=12) 
-    plt.grid(True)# isso pede para desenhar a grade nos eixos # não é necessário de ter
-
-    # com as listas de x e y pasta coloca-las no grafico
+    plt.grid(True)
+    # grid pede para desenhar a grade nos eixos, mas não é necessário de ter
+    # com as listas de x e y pasta coloca-las no gráfico
     if linha:
         plt.plot(x,y)
     else:
         plt.plot(x,y,'o') #'o' indica que é para mostrar pontos no gráfico
-    plt.show() # sem esse comando, dependendo de onde você programa, não mostrará o gráfico
+    plt.show() 
+    ''' Sem o comando show, dependendo de onde você programa, não mostrará o gráfico. Lembre que acima apenas definimos os métodos e não pedimos para o código fazer de fato o gráfico. Dica: Quando queremos que os comandos abaixo executem apenas quando executamos esse arquivo usamos o bloco __main__'''
 
-# Lembre que acima apenas definimos os métodos, não pedimos
-#  para o código fazer nada
+'''Métodos para MCU: ds = R*theta; x=R*cos theta; y=R*sin theta'''
+def xy_MCU(R):
+    x = lambda theta: R*np.cos(theta)
+    y = lambda theta: R*np.sin(theta)
+    return (x, y)
 
-#Dica: Quando queremos que os comandos abaixo executem apenas
-#      quando executamos esse arquivo usamos o bloco __main__
+def radiano(x):
+    return x*2*np.pi/360
+
+def graus(x):
+    return x*360/(2*np.pi)
+
+def Grafico_circulo(raio=1,mostre_theta=np.pi/4, pontos=360, eixox='X (m)', eixoy='Y (m)', delta=1,grade=False):
+    #funções de X e Y
+    f=xy_MCU(raio)
+    if pontos<100:
+        pontos+=100
+    fig = plt.figure(dpi=100)
+    X, Y =[], []
+    for i in range(pontos):
+        X.append(f[0]( radiano(i) ))
+        Y.append(f[1]( radiano(i) ))
+    plt.ylabel(eixoy, fontsize=12)
+    plt.xlabel(eixox, fontsize=12)
+    plt.grid(grade)
+    plt.plot(X,Y)
+    #mostranto o raio no ângulo escolhido
+    o = [0], [0]
+    p = [raio*np.cos(mostre_theta)], [raio*np.sin(mostre_theta)] 
+    plt.quiver(*o,*p,angles='xy', scale_units='xy',scale=1,color='k',label='Raio=%.1f\nÂngulo=%.1f'%(raio,graus(mostre_theta)))
+    plt.legend(loc="upper left")        
+    plt.show()
 
 if __name__ == "__main__":
-    # Definindo a função como: s = 50 -(g/2)* t**2, g=10 m/s**2
-    s = Posicao_MUV(50,0,-10)
-    # Chamamos a criação do gráfico com 11 pontos a cada 0.3 segundos
-    Grafico(f=s,pontos=11,delta=0.3)
-    # Caso queira ver quando a altura zera: 
-    #Grafico(f=s,pontos=317,delta=0.01,linha=True)
-    '''
-Atividade 5:
-Um trabalhador mora a 2,4 km de distância do 
+    # MUV
+    #s = Posicao_MUV(0,15,-10)
+    #Grafico(f=s,pontos=300,delta=0.01,linha=True)
+    #MCU
+    Grafico_circulo(raio=2,grade=True)    
+    
+    '''Atividade 5-MUV: Um trabalhador mora a 2,4 km de distância do 
 seu emprego. Ele tem que decidir entre duas 
 opções de transporte para chegar ao trabalho: 
 de ônibus, cuja velocidade média em sua região 
@@ -97,4 +106,10 @@ bem em frente à sua casa e outro ponto em frente
 ao seu trabalho, e desconsiderando eventuais perdas 
 de tempo na espera do ônibus, qual das opções de
 meio de transporte é mais rápido? Indique com gráfico.
+
+Atividade 5-MCU: Considere uma polia girando em torno de
+seu eixo central, conforme figura abaixo. A velocidade
+dos pontos A e B são, respectivamente, 60 cm/s e 0,3 m/s.
+A distância AB vale 10 cm. O diâmetro e a velocidade
+angular da polia, respectivamente, valem:
 '''
