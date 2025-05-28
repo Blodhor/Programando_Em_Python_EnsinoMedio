149bb03c96871f3ca3786d300a0c8d2081f132bf
Bruno Braga <bruno.braga@ufms.br>
2025-05-28 09:44:25 -0400

    Update and rename Atividade_3_2ano.py to Atividade_3.py

--- 2Ano/Atividade_3.py
+++ 2Ano/Atividade_3.py
@@ -5,7 +5,7 @@ Sendo a distância entre duas cristas sucessivas igual a
 40 cm, teremos que a frequência e a velocidade de pro-
 pagação dessas ondas são, respectivamente, iguais a?
 '''
-from Aula_3_2ano import Periodo, Frequencia
+from Aula_3 import Periodo, Frequencia
 
 # A velocidade de propagação foi dada como 5m/10s
 v = 5/10 #m/s
